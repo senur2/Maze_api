ca4a0f867183d92d5a4ead227354ec3af788b5a0
lalecu <luca.alecu@etu.univ-cotedazur.fr>
2026-02-13 15:34:44 +0100

    ultima 2

--- Src/Main.java
+++ Src/Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args) {
         try {
             Grille grille = new Grille(30, 30);
-            grille.sidewing(50, 5);
+            grille.sidewing(45, 5);
+            grille.smartBraid();
             Labirinthe lab = Utilitaire.genererLabirinthe(grille);
             Benchmark benchmark = new Benchmark(grille);
             benchmark.print();
+            Utilitaire.renderPacman(lab, 10);
             grille.renderMaze(20);
             lab.renderGraphe(20);
         } catch (Exception e) {
